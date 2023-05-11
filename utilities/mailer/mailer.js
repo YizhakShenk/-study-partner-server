607ff883b77005e3e29abcce60025914470adc3f
Yizhak Shenk <89095503+YizhakShenk@users.noreply.github.com>
2023-05-11 20:00:04 +0300

    Create mailer.js

---
+++ utilities/mailer/mailer.js
@@ -0,0 +1,36 @@
+const nodeMailer = require('nodemailer');
+
+const mailTransporter = nodeMailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL_URL,
+        pass: process.env.EMAIL_PASS,
+    }
+});
+
+const transferMail = async (emailDestination, titleMessage, bodyMessage,htmlBody) => {
+    try {
+        console.log('CALLED MAILER');
+        const sent = await mailTransporter.sendMail(
+            {
+                from: 'study-partner',
+                to: emailDestination, 
+                subject: titleMessage,
+                text: bodyMessage,
+                html: htmlBody,
+            },
+        );
+        console.log(sent);
+            console.log("EMAIL SENT");
+            return 'email sent';
+    }
+    catch (err) {
+        console.log("err", err);
+        return err
+    }
+
+}
+
+module.exports = {
+    transferMail,
+}
