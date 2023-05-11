1f85fc35c5335d0a1173390c0f2797ef0b9c9977
Yizhak Shenk <89095503+YizhakShenk@users.noreply.github.com>
2023-05-11 20:02:49 +0300

    Create validations.js

---
+++ utilities/validations/validations.js
@@ -0,0 +1,30 @@
+
+
+const nameRegex = /^[a-zA-Z ]{2,30}$/;
+const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,16}$/;
+const countryRegex = /^[a-zA-Z ]{3,30}$/
+const languagesRegex = /^[a-zA-Z ]{3,20}$/
+const phoneNumberRegex = /^[+ 0-9]{7,14}$/
+const ageRegex = /^[0-9]{2,2}$/
+
+
+
+const nameValid = (name) => { return nameRegex.test(name) }
+const emailValid = (email) => { return emailRegex.test(email) }
+const passwordValid = (password) => { return passwordRegex.test(password) }
+const countryValid = (country) => { return countryRegex.test(country)  }
+const languagesValid = (languages) => { return languagesRegex.test(languages) }
+const phone_numberValid = (phone_number) => { return phoneNumberRegex.test(phone_number) }
+const age_rangeValid = (age) => { return ageRegex.test(age) && (age >16 && age < 100) }
+
+
+module.exports = {
+    nameValid,
+    emailValid,
+    passwordValid,
+    countryValid,
+    languagesValid,
+    phone_numberValid,
+    age_rangeValid,
+}
