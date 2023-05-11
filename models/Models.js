ba618c35cf9f8725b0e23cdd503fadb6384d07d6
Yizhak Shenk <89095503+YizhakShenk@users.noreply.github.com>
2023-05-11 19:49:18 +0300

    Create Models.js

---
+++ models/Models.js
@@ -0,0 +1,21 @@
+
+const RateModel = require('./Rate');
+const PostModel = require('./Post');
+const CategoryModel = require('./Category');
+const SubjectModel = require('./Subject');
+const UserModel = require('./User');
+const UserSubject = require('./UserSubject');
+const NotificationModel = require('./Notifications');
+const NewPostAlert =require('./NewPostAlert');
+
+
+module.exports = {
+    UserSubject,
+    PostModel,
+    CategoryModel,
+    SubjectModel,
+    UserModel,
+    NotificationModel,
+    NewPostAlert,
+    RateModel,
+}
