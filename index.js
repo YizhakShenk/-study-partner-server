2cd1f1b1f68729ec69edee5788d4f75847ca7b3d
Yizhak Shenk <89095503+YizhakShenk@users.noreply.github.com>
2023-05-11 19:34:49 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const db = require('./db/mysql');
+const {fillCategoryTable,fillSubjectTable,fillUserAndPostst} = require('./utilities/dbUtilities/fillTables');
+const express = require('express');
+const cookieParser = require('cookie-parser')
+const app = express();
+const router = require('./routers/index');
+const testRouter = require('./tests/testDB');
+const cors = require('cors');
+
+const cron = require('node-cron');
+const removeOldPosts = require('./utilities/cronJob/cronJob');
+
+app.use(cors({ origin: ['http://localhost:3000', 'http://localhost:3001'], credentials: true }));
+app.use(express.json());
+app.use(cookieParser())
+app.use(router);
+app.use(testRouter);
+
+cron.schedule("0 00 * * *",removeOldPosts);
+
+app.listen(process.env.PORT, () => {
+    (async () => {
+        await db.sync();
+        await fillCategoryTable();
+        await fillSubjectTable();
+        // console.log(await fillUserAndPostst());/// for testing
+        
+    })()
+    console.log(`app listen on port ${process.env.PORT}`)
+});
+
