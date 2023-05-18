c5de860721a3dbf432b3fbde465a288a3f07dd41
Yizhak Shenk <89095503+YizhakShenk@users.noreply.github.com>
2023-05-18 20:21:18 +0300

    Update Models.js

--- models/Models.js
+++ models/Models.js
@@ -2,8 +2,8 @@
 const RateModel = require('./Rate');
 const PostModel = require('./Post');
 const CategoryModel = require('./category');
-const SubjectModel = require('./Subject');
-const UserModel = require('./User');
+const SubjectModel = require('./subject');
+const UserModel = require('./user');
 const UserSubject = require('./UserSubject');
 const NotificationModel = require('./Notifications');
 const NewPostAlert =require('./NewPostAlert');
