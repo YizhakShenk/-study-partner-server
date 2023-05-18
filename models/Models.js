cc850d4038edc5f9b9bbbf92ac4aa21da2a05f62
Yizhak Shenk <1997shenk@gmail.com>
2023-05-18 20:25:32 +0300

    BLA2

--- models/Models.js
+++ models/Models.js
@@ -1,21 +1,19 @@
-
-const RateModel = require('./Rate');
-const PostModel = require('./Post');
-const CategoryModel = require('./category');
-const SubjectModel = require('./subject');
-const UserModel = require('./user');
-const UserSubject = require('./UserSubject');
-const NotificationModel = require('./Notifications');
-const NewPostAlert =require('./NewPostAlert');
-
+const RateModel = require("./Rate");
+const PostModel = require("./Post");
+const CategoryModel = require("./Category");
+const SubjectModel = require("./Subject");
+const UserModel = require("./User");
+const UserSubject = require("./UserSubject");
+const NotificationModel = require("./Notifications");
+const NewPostAlert = require("./NewPostAlert");
 
 module.exports = {
-    UserSubject,
-    PostModel,
-    CategoryModel,
-    SubjectModel,
-    UserModel,
-    NotificationModel,
-    NewPostAlert,
-    RateModel,
-}
+  UserSubject,
+  PostModel,
+  CategoryModel,
+  SubjectModel,
+  UserModel,
+  NotificationModel,
+  NewPostAlert,
+  RateModel,
+};
--- tests/testDB.js
+++ tests/testDB.js
@@ -1,75 +1,66 @@
-const testRouter = require('express').Router();
-const { fillTable } = require('./fillTables')
+const testRouter = require("express").Router();
+const { fillTable } = require("./fillTables");
 
-const PostModel = require('../models/Post');
-const UserModel = require('../models/User');
-const SubjectModel = require('../models/Subject');
-const CategoryModel = require('../models/category');
+const PostModel = require("../models/Post");
+const UserModel = require("../models/User");
+const SubjectModel = require("../models/Subject");
+const CategoryModel = require("../models/Category");
 
-testRouter.get('/test/fill-tables', async(req, res) => {
-    console.log('called fill tables');
-    try {
-        const result = await fillTable();
-        if (result == 'yes') {
-            res.status(200).send('success');
-        }
-        else {
-            throw new Error({ message: 'error' });
-        }
-
+testRouter.get("/test/fill-tables", async (req, res) => {
+  console.log("called fill tables");
+  try {
+    const result = await fillTable();
+    if (result == "yes") {
+      res.status(200).send("success");
+    } else {
+      throw new Error({ message: "error" });
     }
-    catch (err) {
-        res.status(400).send(err)
-    }
-})
-
-
-testRouter.get('/test/get-users', async (req, res) => {
-    console.log('called');
-    try {
-        const result = await UserModel.findAll({ include:PostModel})  //SubjectModel
-        console.log(result);
-        res.status(200).send(result)
-    }
-    catch (err) {
-        res.send(err)
-    }
-})
-testRouter.get('/test/get-subjects', async (req, res) => {
-    console.log('called');
-    try {
-        const result = await SubjectModel.findAll();
-        console.log(result);
-        res.status(200).send(result)
-    }
-    catch (err) {
-        res.send(err)
-    }
-})
-
-
-testRouter.get('/test/get-categories', async (req, res) => {
-    console.log('called');
-    try {
-        const result = await CategoryModel.findAll({include:SubjectModel})
-        console.log(result);
-        res.status(200).send(result)
-    }
-    catch (err) {
-        res.send(err)
-    }
+  } catch (err) {
+    res.status(400).send(err);
+  }
 });
 
-testRouter.get('/test/get-posts', async (req, res) => {
-    console.log('called');
-    try {
-        const result = await PostModel.findAll()
-        console.log(result);
-        res.status(200).send(result)
-    }
-    catch (err) {
-        res.send(err)
-    }
+testRouter.get("/test/get-users", async (req, res) => {
+  console.log("called");
+  try {
+    const result = await UserModel.findAll({ include: PostModel }); //SubjectModel
+    console.log(result);
+    res.status(200).send(result);
+  } catch (err) {
+    res.send(err);
+  }
+});
+testRouter.get("/test/get-subjects", async (req, res) => {
+  console.log("called");
+  try {
+    const result = await SubjectModel.findAll();
+    console.log(result);
+    res.status(200).send(result);
+  } catch (err) {
+    res.send(err);
+  }
 });
 
-module.exports = testRouter
+testRouter.get("/test/get-categories", async (req, res) => {
+  console.log("called");
+  try {
+    const result = await CategoryModel.findAll({ include: SubjectModel });
+    console.log(result);
+    res.status(200).send(result);
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+testRouter.get("/test/get-posts", async (req, res) => {
+  console.log("called");
+  try {
+    const result = await PostModel.findAll();
+    console.log(result);
+    res.status(200).send(result);
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+module.exports = testRouter;
