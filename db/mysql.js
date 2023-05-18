4a5a0467b955f3e80c91403bc111148121720bd0
Yizhak Shenk <89095503+YizhakShenk@users.noreply.github.com>
2023-05-18 21:57:30 +0300

    Update mysql.js

--- db/mysql.js
+++ db/mysql.js
@@ -6,7 +6,7 @@ const {Sequelize} = require('sequelize');
 const db = new Sequelize(dbConfig.DB,dbConfig.USER,dbConfig.PASSWORD,
     {
     host:dbConfig.HOST,
-    dialect:dbConfig.DIALECT,
+//     dialect:dbConfig.DIALECT,
 }
 );
 
